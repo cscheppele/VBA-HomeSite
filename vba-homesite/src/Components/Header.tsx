fe34caa1c06be3934922c6569d210f1c3295e15d
CScheppele <cmscheppele@gmail.com>
2024-05-24 16:52:40 -0500

    adding style files renamed styles and components folders

--- vba-homesite/src/Components/Header.tsx
+++ vba-homesite/src/Components/Header.tsx
@@ -1,15 +1,15 @@
-import '../Styling/Header.module.css';
+import styles from '../styles/Header.module.css';
 import React from 'react';
 
 function Header () {
     return(
-        <div className="Main">
+        <div className={styles.Main}>
             <img src="../public/VBA-header-logo.jpeg"/>
-            <div>Home</div>
-            <div>Awards</div>
-            <div>Calendar</div>
-            <div>Board</div>
-            <div>Contact</div>
+            <div className={styles.Link}>Home</div>
+            <div className={styles.Link}>Awards</div>
+            <div className={styles.Link}>Calendar</div>
+            <div className={styles.Link}>Board</div>
+            <div className={styles.Link}>Contact</div>
         </div>
     )
 };
