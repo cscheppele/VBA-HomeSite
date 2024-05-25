349848948f2e89fe0444a983800d7f3519d5623d
CScheppele <cmscheppele@gmail.com>
2024-05-24 21:38:42 -0500

    todays update for header

--- vba-homesite/src/Components/Header.tsx
+++ vba-homesite/src/Components/Header.tsx
@@ -4,12 +4,19 @@ import React from 'react';
 function Header () {
     return(
         <div className={styles.Main}>
-            <img src="../public/VBA-header-logo.jpeg"/>
-            <div className={styles.Link}>Home</div>
-            <div className={styles.Link}>Awards</div>
-            <div className={styles.Link}>Calendar</div>
-            <div className={styles.Link}>Board</div>
-            <div className={styles.Link}>Contact</div>
+            <img className={styles.Logo} src="/VBA-header-logo.jpeg"/>
+            <div className={styles.LinkBox}>
+                <div className={styles.Link}>Home</div>
+                <div className={styles.Link}>Awards</div>
+                <div className={styles.Link}>Calendar</div>
+                <div className={styles.Link}>Board</div>
+                <div className={styles.Link}>Contact</div>
+            </div>
+            <div className={styles.SignUpBox}>
+                <div>Sign Up</div>
+                <div>/</div>
+                <div>Sign In</div>
+            </div>
         </div>
     )
 };
