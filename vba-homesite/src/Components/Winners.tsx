302c856bc7791cf46b103222a9fc903aa66a3f05
CScheppele <cmscheppele@gmail.com>
2024-06-02 17:30:29 -0500

    started building on click function for winners component

--- vba-homesite/src/Components/Winners.tsx
+++ vba-homesite/src/Components/Winners.tsx
@@ -9,9 +9,26 @@ export default function Winners () {
     //or should all of them be loaded in and you can just scroll through them?
     //function to rotate currentDiv through the the divisions array?
     //or am I just changing the position of each one?
+    function changeDivision() {
+        // if(){
+        //     if(divisions.indexOf(currentDiv) == 0) {
+        //         currentDiv = divisions[divisions.length - 1];
+        //     } else {
+        //     currentDiv = divisions[divisions.indexOf(currentDiv)-1]
+        //     }
+        // } else {
+        //     if(divisions.indexOf(currentDiv) == divisions.length - 1) {
+        //         currentDiv = divisions[0];
+        //     } else {
+        //     currentDiv = divisions[divisions.indexOf(currentDiv)+1]
+        //     }
+        // }
+    }
     return (
         <div className={styles.main}>
+            {/*left arrow button for on click functionality*/}
             <DivisionPanel/>
+            {/**Right arrow button for an on click functionality */}
             {/*need to research the forEach way of displaying an array again */}
             {/* <DivisionPanel currentDiv={currentDiv}/> */}
         </div>
