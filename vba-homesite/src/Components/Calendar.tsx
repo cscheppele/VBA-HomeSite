44cf3e8603f0a5aff2b534767eb1c398890d28b1
CScheppele <cmscheppele@gmail.com>
2024-06-01 22:30:50 -0500

    super small outline of calendar

--- vba-homesite/src/Components/Calendar.tsx
+++ vba-homesite/src/Components/Calendar.tsx
@@ -1,8 +1,8 @@
 import styles from '../styles/Calendar.module.css';
 
 export default function Calendar () {
-
+    //ask Anna and juliana how to go about importing a calendar
     return (
-        <div></div>
+        <div className={styles.main}>Here we can import the VBA calendar</div>
     )
 }
