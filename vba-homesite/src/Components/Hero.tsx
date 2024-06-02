73b3dca3ea586393dcce74c6a5cf8a9f2861d182
CScheppele <cmscheppele@gmail.com>
2024-06-01 20:17:50 -0500

    header base outline

--- vba-homesite/src/Components/Hero.tsx
+++ vba-homesite/src/Components/Hero.tsx
@@ -1,6 +1,13 @@
-import '../Styling/Hero.module.css';
+import styles from '../styles/Hero.module.css';
 
 function Hero () {
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.heroPic}>this is a place holder for a photo</div>
+            <p className={styles.mission}>Volleyball Austin offers the Central Texas lesbian, gay, bisexual, transgender (LGBT+) community and our straight allies the opportunity to experience and enjoy volleyball in an inclusive and affirming environment.</p>
+        </div>
+    )
 
 }
 
